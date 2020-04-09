29ca29003e109012e84da2abc88a6bd6e79a2f02
Asadhjafri <asadhjafri@gmail.com>
2020-04-09 12:02:03 -0400

    created interface and started creating main screen of app

---
+++ lib/model/interface.rb
@@ -0,0 +1,14 @@
+class Interface
+
+    def start_up
+    end
+
+    def welcome
+        puts "Welcome to Gotham Motors"
+    end
+
+    def login_or_join
+        puts "Are you and existing user, or will you be signing up?"
+    end
+
+end
